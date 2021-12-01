77b4e1c91e0e696d2cb7f40718df9755170c3bcd
Lourdes Cornejo Adrianzen <mcornejoa1@upao.edu.pe>
2021-12-01 08:10:38 -0500

    (frontend) Maquetación de Register

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 
 import {  MatButtonModule } from '@angular/material/button';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
     imports:[
-        MatButtonModule
+        MatButtonModule,
+        MatFormFieldModule
         
     ],
 
     exports: [
-        MatButtonModule
+        MatButtonModule,
+        MatFormFieldModule
     ]
 })
 
