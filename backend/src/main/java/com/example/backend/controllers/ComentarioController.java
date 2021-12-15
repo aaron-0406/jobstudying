553afdd1435a04b5742cdf77338e89e7f477b780
noemi <gvasquezr4@upao.edu.pe>
2021-12-15 01:42:45 -0500

    (backend)implementacion comentario Rest

---
+++ backend/src/main/java/com/example/backend/controllers/ComentarioController.java
@@ -0,0 +1,30 @@
+package com.example.backend.controllers;
+
+
+import com.example.backend.model.Comentario;
+import com.example.backend.services.ComentarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/comentario")
+public class ComentarioController {
+
+    @Autowired
+    private ComentarioService comentarioService;
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<Comentario> findComentario(@PathVariable("id")Integer id){
+        Comentario comentario= comentarioService.findComentario(id);
+        if(comentario==null) return  ResponseEntity.noContent().build();
+        return ResponseEntity.ok(comentario);
+    }
+
+    @PostMapping
+    public ResponseEntity <Comentario> createComentario(@RequestBody Comentario comentario){
+        Comentario comentarioCreate= comentarioService.createComentario(comentario);
+        return ResponseEntity.ok(comentarioCreate);
+
+    }
+}
---
+++ backend/src/main/java/com/example/backend/repositories/ComentarioRepository.java
@@ -0,0 +1,15 @@
+package com.example.backend.repositories;
+
+import com.example.backend.model.Comentario;
+import com.example.backend.model.Oferta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ComentarioRepository extends JpaRepository<Comentario, Integer> {
+
+
+    List<Comentario> findByOferta(Integer idOferta);
+}
